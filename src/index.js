da6dc39f38ae3f50dd41e29d444f60a9bd6315c2
Evgenia <zhdanovichevgenia@gmail.com>
2017-09-16 15:34:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function zeros(expression) {
-  // your solution
+   var arr = expression.split('*');
+ var arrOfFactorials = [];
+ var finalResult = 1;
+ for(var i = 0;i < arr.length;i++){
+	var temp = null;
+	var subArr = arr[i].split('');
+	for(var j = 0; j < subArr.length; j++){
+		if(subArr[j] === '!'){
+			temp++;
+		}
+	}
+	var numberToFact = parseInt(arr[i]);
+	switch(temp){
+		case 1: arrOfFactorials.push(factorial(numberToFact));break;
+		case 2: arrOfFactorials.push(factorialEven(numberToFact));break;
+	}
+	
+ }
+		for(var k = 0;k < arrOfFactorials.length;k++){
+			finalResult*=arrOfFactorials[k];
+		}
+		return finalResult;
+ 
+ function factorial(n) {
+  return n ? n * factorial(n - 1) : 1;
+}
+function factorialEven(n) {
+	if(n === 1){
+		return 1;
+	}
+	var result = 1;
+	while(n > 0){
+		result*=n;
+		n-=2;
+	}
+	return result;
+}
 }
